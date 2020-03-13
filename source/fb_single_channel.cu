1fa348b1f1163046a48de2bbc3728d316f9516f4
dirog <diroges@gmail.com>
2020-03-13 23:31:30 +0300

    modified version of single channel fb

--- source/fb_single_channel.cu
+++ source/fb_single_channel.cu
@@ -4,58 +4,32 @@
 #include <cufftXt.h>
 #include "device_launch_parameters.h"
 
-#define DIM 1024
-
 cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsigned filterLen, unsigned fftSize, unsigned step, float* result, unsigned resultLen);
 void readVectorFromFile(const char* fileName, float* result, int len);
 void writeResultToFile(const char* fileName, float* result, int x, int y);
-__device__ cufftComplex operator + (cufftComplex const& a, cufftComplex const& b);
+__inline__ __device__ cufftComplex operator + (cufftComplex const& a, cufftComplex const& b);
 
-
-__global__ void multiply(cufftComplex* signalVec, float* filterTaps, cufftComplex* vecOut, int size, int step, int stepIndx)
+__global__ void mupltiply_sum(cufftComplex* signal, cufftComplex* resultVec, float* filterTaps, int k, int step)
 {
-    unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
-    if (i < size / step) {
-        int indx = step * i + stepIndx;
-        vecOut[i].x = signalVec[indx].x * filterTaps[indx];
-        vecOut[i].y = signalVec[indx].y * filterTaps[indx];
-    }
-}
+    int index = blockIdx.x * step + threadIdx.x;
+    int res_index = blockIdx.x * blockDim.x + threadIdx.x;
+    cufftComplex result;
+    result.x = 0;
+    result.y = 0;
 
-__global__ void reductionSum(cufftComplex* vect, cufftComplex* vecOut, int size, int fftSize, int stepIndx)
-{
-    __shared__ cufftComplex block[DIM];
-    unsigned int globalIndex = threadIdx.x + blockDim.x * blockIdx.x;
-    unsigned int i = threadIdx.x;
-
-    if (globalIndex < size) {
-        block[i] = vect[globalIndex];
-    }
-    else {
-        block[i].x = 0;
-        block[i].y = 0;
-    }
-
-    __syncthreads();
-
-    for (unsigned int j = blockDim.x / 2; j > 0; j >>= 1)
+    for (int i = 0; i < k; ++i)
     {
-        if (i < j){
-            block[i] = block[i] + block[i + j];
-        }
-
-        __syncthreads();
+        result.x += filterTaps[i * blockDim.x + threadIdx.x] * signal[i * blockDim.x + index].x;
+        result.y += filterTaps[i * blockDim.x + threadIdx.x] * signal[i * blockDim.x + index].y;
     }
 
-    if (i == 0){
-        vecOut[blockIdx.x] = block[0];
-    }
-
+    resultVec[res_index] = result;
 }
+
 
 
 int main() {
-    const int signalLen = 1024*8*2;
+    const int signalLen = 1024 * 8 * 2;
     const int filterLen = 128;
     const int fftSize = filterLen / 16;
     const int step = 32;
@@ -83,17 +57,12 @@ int main() {
 
 cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsigned filterLen, const unsigned fftSize, unsigned step, float* result, unsigned resultLen)
 {
-    int threadsPerBlock = DIM;
-
     float* dev_inSignal;
     float* dev_filterTaps;
     cufftComplex* dev_result;
-    cufftComplex* dev_subVec;
-    cufftComplex* dev_vecOut;
 
     int fftCount = ((signalLen / 2 - filterLen) / step) + 1;
-    int subVecSize = filterLen / fftSize;
-    
+
     cudaError_t cudaStatus;
     cufftResult cufftStatus;
 
@@ -121,18 +90,6 @@ cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsi
         return cudaStatus;
     }
 
-    cudaStatus = cudaMalloc((float**)&dev_subVec, filterLen * sizeof(float));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!\n");
-        return cudaStatus;
-    }
-
-    cudaStatus = cudaMallocManaged((float**)&dev_vecOut, subVecSize * sizeof(float));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!\n");
-        return cudaStatus;
-    }
-
     cudaStatus = cudaMemcpy(dev_inSignal, inSignal, signalLen * sizeof(float), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!\n");
@@ -146,40 +103,8 @@ cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsi
     }
 
     cufftComplex* dev_inComplexSignal = reinterpret_cast<cufftComplex*>(dev_inSignal);
-    int numInputElements = subVecSize;
-    int numOutputElements;
-    
-    for (int batchIndx = 0; batchIndx < fftCount; batchIndx++)
-    {
-        for (int stepIndx = 0; stepIndx < fftSize; stepIndx++) {
-            multiply << <256, threadsPerBlock >> > (dev_inComplexSignal + batchIndx*step, dev_filterTaps, dev_subVec, filterLen, fftSize, stepIndx);
-            do
-            {
-                numOutputElements = numInputElements / (threadsPerBlock);
-                if (numInputElements % (threadsPerBlock)) {
-                    numOutputElements++;
-                }
-
-                reductionSum << < numOutputElements, threadsPerBlock >> > (dev_subVec, dev_vecOut, numInputElements, fftSize, stepIndx);
-                numInputElements = numOutputElements;
-                if (numOutputElements > 1) {
-                    reductionSum << < numOutputElements, threadsPerBlock >> > (dev_vecOut, dev_subVec, numInputElements, fftSize, stepIndx);
-                }
-
-            } while (numOutputElements > 1);
-
-            cudaStatus = cudaDeviceSynchronize();
-            if (cudaStatus != cudaSuccess) {
-                fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching Kernel!\n", cudaStatus);
-                return cudaStatus;
-            }
-
-            dev_result[stepIndx + batchIndx*fftSize] = dev_vecOut[0];
-            dev_vecOut[0].x = 0;
-            dev_vecOut[0].y = 0;
-            numInputElements = subVecSize;
-        }
-    }
+   
+    mupltiply_sum << <fftCount, fftSize >> > (dev_inComplexSignal, dev_result, dev_filterTaps, filterLen / fftSize, step);
 
     cufftHandle plan;
     cufftStatus = cufftPlan1d(&plan, fftSize, CUFFT_C2C, fftCount);
@@ -194,8 +119,6 @@ cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsi
         fprintf(stderr, "cufftExecC2C failed. Error code %d!\n", cufftStatus);
         return cudaErrorUnknown;
     }
-
-
 
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
@@ -218,9 +141,7 @@ cudaError_t execute(float* inSignal, unsigned signalLen, float* filterTaps, unsi
 
     cudaFree(dev_inSignal);
     cudaFree(dev_filterTaps);
-    cudaFree(dev_subVec);
     cudaFree(dev_result);
-    cudaFree(dev_vecOut);
 
     return cudaStatus;
 }
